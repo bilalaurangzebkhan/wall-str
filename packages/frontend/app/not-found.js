b59c24721c51d277066fb7bb3af37f033cb99610
Bilal Aurangzeb Khan <78090865+bilalaurangzebkhan@users.noreply.github.com>
2025-07-11 21:18:25 +0500

    Create not-found.js

---
+++ packages/frontend/app/not-found.js
@@ -0,0 +1,9 @@
+// app/not-found.js or app/_not-found/page.js
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  )
+}
